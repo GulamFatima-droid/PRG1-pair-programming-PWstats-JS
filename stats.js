f13d3ccb732e11d6649903f0e61c150db01fd966
GulamFatima-droid <gulamfatima.ahmadnaz@ada.ac.uk>
2024-10-01 10:05:07 +0000

    commit

--- stats.js
+++ stats.js
@@ -1,25 +1,82 @@
 const fs = require("fs");
-
+ 
 const inputFile = "10000-most-common-passwords.csv";
 const outputFile = "statistics.csv";
 const delimiter = ",";
-
+const alphabetArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+ 
 function deleteExistingOutputFile() {
   if (fs.existsSync(outputFile)) {
     fs.unlinkSync(outputFile);
   }
 }
-
+ 
 function processData() {
+  let maxLength = 0;
   const data = fs.readFileSync(inputFile, "utf-8");
   const lines = data.split(/\n/);
-
-  for (line of lines) {
+ 
+ 
+  for (const line of lines) {
     elements = line.split(delimiter);
-    console.log(elements);
+    if (elements[1].length > maxLength) {
+      maxLength = elements[1].length;
+    }
   }
-}
 
+  for (let i = 0; i < maxLength; i++) {
+    let count = 0;
+    for (let line of lines) {
+      elements = line.split(delimiter);
+      if (elements[1].length === i) {
+        count += 1;
+      }
+    }
+    fs.appendFileSync(outputFile, `Chars: ${i},Count: ${count}\n`, "utf-8");
+  }
+    
+  for (let i = 0; i < 26; i++) {
+    let count = 0;
+    for (let line of lines) {
+      elements = line.split(delimiter);
+      if (elements[1][0] === alphabetArray[i]) {
+        count += 1;
+      }    
+    }
+    fs.appendFileSync(outputFile, `Chars: ${alphabetArray[i]},Count: ${count}\n`, "utf-8");
+  
+  }
+
+}
+ 
 // Main execution
-deleteExistingOutputFile(); 
+deleteExistingOutputFile();
 processData();
+ 
